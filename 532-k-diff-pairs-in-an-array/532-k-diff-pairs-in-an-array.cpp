e97d485f6a6f29d6cd76602460da73351e2d0d71
Amrutanshu Dash <68193810+AmrutanshuDash@users.noreply.github.com>
2022-02-09 22:11:08 +0530

    Time: 13 ms (89.12%), Space: 11.2 MB (92.85%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int findPairs(vector<int>& nums, int k) {
-		sort(nums.begin(),nums.end());
-		int j=1;
-		int i=0;
-		int ans=0;
-		while(i<nums.size() && j<nums.size()){
-			if(nums[j]-nums[i]==k){
-				ans++;
-				j++;
-				i++;
-				while(j<nums.size() && nums[j]==nums[j-1]){
-					j++;
-				}
-			}
-			else if(nums[j]-nums[i]>k){
-				i++;  
-				if(j-i==0){
-					j++;}
-			}
-			else{      
-				j++;
-			}         
-		}
-		return ans;
-
-	}
+int findPairs(vector<int>& nums, int k) {
+  sort(nums.begin(), nums.end());
+  int i =0; int count =0;
+  int j =1;
+  while(i<nums.size() && j<nums.size()){
+      if(nums[j]-nums[i] == k) {
+          count++; 
+          i++;
+          j++;
+          // to make it unique
+          while(j<nums.size() && nums[j]==nums[j-1] )
+              j++;
+      }
+      else if(nums[j]-nums[i] > k){
+          i++;
+          if(j-i==0){
+              j++;}
+      }
+      else{
+          j++;}
+  }
+  return count;
+  
+}
 };
